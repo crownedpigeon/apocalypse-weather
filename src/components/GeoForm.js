f7d37103caf05f2efa8cd8eef14769da8f7173c8
crownedpigeon <emily.rigole@gmail.com>
2020-09-23 10:30:37 -0400

    use react-geocode to get lat/lng

--- src/components/GeoForm.js
+++ src/components/GeoForm.js
@@ -1,8 +1,23 @@
 import React from "react";
-
+import Geocode from "react-geocode";
+require("dotenv").config();
 // import "./GeoForm.css";
+console.log(process.env);
+Geocode.setApiKey(process.env.REACT_APP_GEOCODING_API_KEY);
 
 const GeoForm = ({ setLatLng }) => {
+  const getLatLng = () => {
+    Geocode.fromAddress("Atlanta").then(
+      response => {
+        const { lat, lng } = response.results[0].geometry.location;
+        console.log(lat, lng);
+      },
+      error => console.error(error)
+    );
+  };
+
+  getLatLng();
+
   return (
     <form className="form">
       <label>Enter location: </label>
