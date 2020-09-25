dfff853121ba21be0db48dca6c982a693e6b9003
crownedpigeon <emily.rigole@gmail.com>
2020-09-25 11:30:00 -0400

    using location to get weather data

--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
+require("dotenv").config();
 
-const WeatherDisplay = () => {
-  return <h2>weather chart</h2>;
+const WeatherDisplay = ({ latLng }) => {
+  useEffect(() => {
+    const getWeather = async ({ lat, lng }) => {
+      const res = await fetch(
+        `${process.env.REACT_APP_OPENWEATHER_DOMAIN}onecall?lat=${lat}&lon=${lng}&exclude=hourly,minutely&appid=${process.env.REACT_APP_OPENWEATHER_API_KEY}`
+      );
+      const data = await res.json();
+      console.log(data);
+    };
+
+    getWeather(latLng);
+  }, []);
+
+  const apocalypticWeather = {
+    200: "death reigns from above",
+    300: "blood rain",
+    500: "toxic rain",
+    600: "post-nuclear cold front",
+    620: "blizzards"
+  };
+
+  return <h2>{latLng.lat}</h2>;
 };
 
 export default WeatherDisplay;
