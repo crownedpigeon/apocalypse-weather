10b5a601edba71ebed59ff1666798c5fe22cd04f
crownedpigeon <emily.rigole@gmail.com>
2020-10-12 10:03:00 -0400

    animate radiation storm

--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -6,11 +6,27 @@ import { ReactComponent as Miasma } from "../icons/miasma.svg";
 import { ReactComponent as Radiation } from "../icons/radiation.svg";
 import { ReactComponent as Sun } from "../icons/sun.svg";
 import { ReactComponent as Unnatural } from "../icons/unnatural.svg";
-import { TweenLite } from "gsap";
+import { TimelineLite } from "gsap";
 
 class WeatherDisplay extends React.Component {
+  constructor(props) {
+    super(props);
+    this.lightningFlash = null;
+    this.stormShake = null;
+  }
+
   componentDidMount = () => {
-    TweenLite.to(".radiation", 2, { fill: "red" });
+    this.lightningFlash = new TimelineLite({ repeat: -1 })
+      .to(".flash", 0.2, { opacity: 0 })
+      .to(".flash", 0.2, { opacity: 1 })
+      .to(".flash", 0.2, { opacity: 0 })
+      .to(".flash", 0.2, { opacity: 1 })
+      .to(".flash", 0.2, { opacity: 0 })
+      .to(".flash", 1, { opacity: 0 });
+
+    this.stormShake = new TimelineLite({ repeat: -1 })
+      .to(".radiation-storm", 0.2, { y: 5, x: 2 })
+      .to(".radiation-storm", 0.2, { y: 0, x: -2 });
   };
 
   render() {
